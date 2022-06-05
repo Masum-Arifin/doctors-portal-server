f2b0dc6a53806b54a588138acc6c873b0bfc35e2
Masum-Arifin <masumarifinbd@gmail.com>
2022-06-05 19:32:23 +0600

    uri

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8a1ny7r.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
   
 app.get('/', (req, res) =>{
     res.send('Hello from Doctor uncle')
