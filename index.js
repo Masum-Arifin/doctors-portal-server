8c03f04ed29dc06b5a469bc50cf382d11b56c41d
Masum-Arifin <masumarifinbd@gmail.com>
2022-06-05 21:52:47 +0600

    service

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,7 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://doctor_admin:${process.env.DB_PASS}@cluster0.8a1ny7r.mongodb.net/?retryWrites=true&w=majority`;
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8a1ny7r.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,32 +19,30 @@ const client = new MongoClient(uri, {
 });
 
 
-async function run(){
-    try{
-        await client.connect();
-        const serviceCollection = client.db('doctors_portal').collection('services');
+async function run() {
+  try {
+    await client.connect();
+    console.log('database connected');
+    const serviceCollection = client
+      .db("doctors_portal")
+      .collection("services");
 
-        app.get('/service', async(req, res) =>{
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
-        })
-
-
-    }
-    finally{
-
-    }
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
 }
 
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello From Doctor Uncle!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello From Doctor Uncle!");
+});
 
 app.listen(port, () => {
-  console.log(`Doctors App listening on port ${port}`)
-})
+  console.log(`Doctors App listening on port ${port}`);
+});
