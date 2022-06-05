7811df22ec63435c6dd6884ca49ca91ed48aeb06
Masum-Arifin <masumarifinbd@gmail.com>
2022-06-05 19:50:18 +0600

    client

--- index.js
+++ index.js
@@ -11,7 +11,20 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8a1ny7r.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+       await client.connect(); 
+       console.log('Database connected');
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
   
 app.get('/', (req, res) =>{
     res.send('Hello from Doctor uncle')
