fd7f81f29ec0e9eeeb08ea49871b7a89cc59f29b
Masum-Arifin <masumarifinbd@gmail.com>
2022-06-05 22:50:28 +0600

    service

--- index.js
+++ index.js
@@ -9,20 +9,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8a1ny7r.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xhqao28.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
-
 async function run() {
   try {
     await client.connect();
-    console.log('database connected');
+    console.log("database connected");
     const serviceCollection = client
       .db("doctors_portal")
       .collection("services");
