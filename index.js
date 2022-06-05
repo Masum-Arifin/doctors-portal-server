330bd9bc6e6c28295a94c4d8e7b3194fa98ea636
Masum-Arifin <masumarifinbd@gmail.com>
2022-06-05 19:05:44 +0600

    get and post

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// index.js- get-and-post
-const express = require('express')
-const cors = require('cors');
-require('dotenv').config();
-const app = express()
-const port = process.env.PORT || 5000;
+// // index.js- get-and-post
+// const express = require('express')
+// const cors = require('cors');
+// require('dotenv').config();
+// const app = express()
+// const port = process.env.PORT || 5000;
 
-// middleware
-app.use(cors());
-app.use(express.json());
+// // middleware
+// app.use(cors());
+// app.use(express.json());
   
-app.get('/', (req, res) =>{
-    res.send('Hello from Doctor uncle')
-})
+// app.get('/', (req, res) =>{
+//     res.send('Hello from Doctor uncle')
+// })
 
-app.listen(port, () =>{
-    console.log('Doctor App Listening to port', port);
-})
+// app.listen(port, () =>{
+//     console.log('Doctor App Listening to port', port);
+// })
