23b96d8f752b8ce62a31d92737c61c69f2e1cd26
Masum-Arifin <masumarifinbd@gmail.com>
2022-06-30 15:23:20 +0600

    booking

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ if(exits){
   return res.send({success: false, booking: exits})
 }
   const result = await bookingCollection.insertOne(booking);
-  res.send({successresult});
+  res.send({success result});
 })
 
     });
